c9398f7fa0c227d556dc712ac03fbd79609d2908
aaronz@vt.edu <aaronz@vt.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-10-18 17:28:02 +0000

    SAK-11913: Committed the non-working test stubs since I had to abandon the old tests
    git-svn-id: https://source.sakaiproject.org/svn/memory/branches/SAK-11913@37113 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ memory-impl/impl/src/test/org/sakaiproject/memory/impl/test/BasicMemoryServiceTest.java
@@ -0,0 +1,86 @@
+/**
+ * BasicMemoryServiceTest.java - memory-impl - 2007 Oct 18, 2007 6:25:17 PM - azeckoski
+ */
+
+package org.sakaiproject.memory.impl.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing the MemoryService
+ * 
+ * @author Aaron Zeckoski (aaron@caret.cam.ac.uk)
+ */
+public class BasicMemoryServiceTest extends TestCase {
+
+   /**
+    * @throws java.lang.Exception
+    */
+   protected void setUp() throws Exception {
+      // TODO
+   }
+
+   /**
+    * @throws java.lang.Exception
+    */
+   protected void tearDown() throws Exception {
+      // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#init()}.
+    */
+   public void testInit() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#destroy()}.
+    */
+   public void testDestroy() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#getAvailableMemory()}.
+    */
+   public void testGetAvailableMemory() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#getStatus()}.
+    */
+   public void testGetStatus() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#newCache(java.lang.String)}.
+    */
+   public void testNewCacheString() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#newCache(java.lang.String, org.sakaiproject.memory.api.CacheRefresher, org.sakaiproject.memory.api.DerivedCache, boolean, boolean)}.
+    */
+   public void testNewCacheStringCacheRefresherDerivedCacheBooleanBoolean() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#resetCachers()}.
+    */
+   public void testResetCachers() {
+      fail("Not yet implemented");
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.BasicMemoryService#doReset()}.
+    */
+   public void testDoReset() {
+      fail("Not yet implemented");
+   }
+
+}
---
+++ memory-impl/impl/src/test/org/sakaiproject/memory/impl/test/MemCacheTest.java
@@ -0,0 +1,114 @@
+/**
+ * MemCacheTest.java - memory-impl - 2007 Oct 18, 2007 6:28:30 PM - azeckoski
+ */
+package org.sakaiproject.memory.impl.test;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Test case for MemCache
+ * 
+ * @author Aaron Zeckoski (aaron@caret.cam.ac.uk)
+ */
+public class MemCacheTest extends TestCase {
+
+   /* (non-Javadoc)
+    * @see junit.framework.TestCase#setUp()
+    */
+   protected void setUp() throws Exception {
+      super.setUp();
+   }
+
+   /* (non-Javadoc)
+    * @see junit.framework.TestCase#tearDown()
+    */
+   protected void tearDown() throws Exception {
+      super.tearDown();
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#MemCache(org.sakaiproject.memory.impl.BasicMemoryService, net.sf.ehcache.Ehcache, org.sakaiproject.memory.api.CacheRefresher, org.sakaiproject.memory.api.DerivedCache)}.
+    */
+   public void testMemCacheBasicMemoryServiceEhcacheCacheRefresherDerivedCache() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#attachDerivedCache(org.sakaiproject.memory.api.DerivedCache)}.
+    */
+   public void testAttachDerivedCache() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#put(java.lang.String, java.lang.Object)}.
+    */
+   public void testPutStringObject() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#put(java.lang.String, java.lang.Object, java.lang.String[])}.
+    */
+   public void testPutStringObjectStringArray() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#containsKey(java.lang.String)}.
+    */
+   public void testContainsKey() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#get(java.lang.String)}.
+    */
+   public void testGet() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#remove(java.lang.String)}.
+    */
+   public void testRemove() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#clear()}.
+    */
+   public void testClear() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#destroy()}.
+    */
+   public void testDestroy() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#resetCache()}.
+    */
+   public void testResetCache() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#getSize()}.
+    */
+   public void testGetSize() {
+      fail("Not yet implemented"); // TODO
+   }
+
+   /**
+    * Test method for {@link org.sakaiproject.memory.impl.MemCache#getDescription()}.
+    */
+   public void testGetDescription() {
+      fail("Not yet implemented"); // TODO
+   }
+
+}
